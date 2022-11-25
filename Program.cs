fd4830c2cbe437937841fa237363d7868088b5dc
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-25 17:17:00 +0700

    add public bool delnode

--- Program.cs
+++ Program.cs
@@ -98,7 +98,21 @@ namespace Exercise3_058
                 Console.Write(LAST.rollNumber + " " +LAST.name+"\n");
             }
         }
-        
+
+        public bool delNode(int nim)
+        {
+            Node previous, current;
+            previous = current = null;
+            //check if the spesified node is present in the list or not
+            if
+                (Search(nim, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == LAST)
+                LAST = current.next;
+            return true;
+        }
+
         public void firstNode()
         {
             if(listEmpty())
