9064fb45f1b88a35520376830431a287f0d3b341
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-25 16:18:30 +0700

    add class circularlist

--- Program.cs
+++ Program.cs
@@ -13,6 +13,30 @@ namespace Exercise3_058
         public string name;
         public Node next;
     }
+
+    class CircularList
+    {
+        Node LAST;
+        public CircularList() 
+        {
+            LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous,ref Node current)
+        //search for the specified node
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true); //return true if node is found
+            }
+            if(rollNo==LAST=LAST.rollNumber) //if yhe node is presents at the end
+                    return true; 
+                else
+                    return (false); //return false if node not found
+        }
+
+    }
     
     internal class Program
     {
