4a87dce815d76f0f91b385ac4a94fee557a6918b
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-25 16:27:23 +0700

    add public void traverse

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace Exercise3_058
                 if (rollNo == current.rollNumber)
                     return (true); //return true if node is found
             }
-            if(rollNo==LAST=LAST.rollNumber) //if yhe node is presents at the end
+            if(rollNo==LAST.rollNumber) //if yhe node is presents at the end
                     return true; 
                 else
                     return (false); //return false if node not found
@@ -43,6 +43,25 @@ namespace Exercise3_058
             else
                 return false;
         }
+
+        public void traverse()  //traverse att the node of the list
+        {
+            if(listEmpty())
+                Console.WriteLine("\n List is Empty");
+            else
+            {
+                Console.WriteLine("\n Records in the list are:\n);
+                node currentNode;
+                currentNode=LAST.next;
+                while(currentNode !=LAST)
+                {
+                    Console.WriteLine(currentNode.rollNumber + "  " + cureentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + " " +LAST.name+"\n");
+            }
+        }
+        
 
     }
     
